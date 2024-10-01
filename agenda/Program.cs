d3041eabfee375ab71a41d09c1c6d0f647f820b5
Victor Emanuel <victorgit28@outlook.com>
2024-10-01 14:41:19 -0300

    Criando novo projeto

---
+++ agenda/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Data.Common;
+
+Console.Clear();
+//Limpando o console
+DateTime dataAtual = DateTime.Now;
+//Criando um objeto instanciado a parti da classe DateTime e utilizando o Now para já retornar a data atual
+Console.WriteLine($"Personalize a sua agenda do dia: {dataAtual.ToString("dd/MM/yyyy")}");
+//toString foi utilizado para exibir somente a data, pois o dataAtual traz a data + a hora (retorno do Now).
