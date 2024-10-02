8a455fd11e04ce39e364a6632613a54ee4b0ccad
Victor Emanuel <victorgit28@outlook.com>
2024-10-02 13:58:50 -0300

    Criando classe

--- agenda/Program.cs
+++ agenda/Program.cs
@@ -1,23 +1,16 @@
-﻿using System.Data.Common;
+﻿using System.Collections.Generic;
+//Chamando bibliotecas necessarias! No caso, a Collections.Generic está sendo chamada para o "list" ser melhor utilizado.
 
-Console.Clear();
-//Limpando o console
-DateTime dataAtual = DateTime.Now;
-//Criando um objeto instanciado a parti da classe DateTime e utilizando o Now para já retornar a data atual
-Console.WriteLine($"Personalize a sua agenda do dia: {dataAtual.ToString("dd/MM/yyyy")}");
-//toString foi utilizado para exibir somente a data, pois o dataAtual traz a data + a hora (retorno do Now).
+public class Tarefa{
+    public int Id {get; set;}
+    public string Nome {get; set;}
+    public string Descricao {get; set;}
+    public bool Check {get; set;}
 
-int esc = 0;
-//Criando variavel para conter a escolha do usuario
-bool escVer = false;
-//Criando variavel para validar se a reposta do user foi satisfatoria
-do{
-    Console.WriteLine("Opções: 1- Gerenciar Tarefas 2- Visualizar Tarefas");
-    //Exibindo opções para o usuario
-    if(int.TryParse(Console.ReadLine(), out esc) && (esc <= 2 && esc >= 1))
-        escVer = true;
-    //Criando um loop que vai ficar repetindo até que o usuario satisfaça a condição
-}while(escVer == false);
-
-
-
+    public Tarefa(int id, string nome, string descricao){
+        Id = id;
+        Nome = nome;
+        Descricao = descricao;
+        Check = false;
+    } 
+}
